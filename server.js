b30a923446b06a1e6f98214b526f8cbd9b9231a0
Addy Osmani <addyosmani@gmail.com>
2016-05-14 06:59:33 -0700

    prep for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = new express(),
-	port = process.env.PORT || 5000;
+	port = process.env.PORT || 8080;
 
 app.set('view engine', 'jade');
 app.set('views', './app/views');
