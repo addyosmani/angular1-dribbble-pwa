42b2fc24c6f2e0305084bd569ec40ffc978d9c16
Addy Osmani <addyosmani@gmail.com>
2016-05-14 08:35:28 -0700

    more fixes

--- app/runtime-caching.js
+++ app/runtime-caching.js
@@ -4,20 +4,20 @@
 // importScripts() call that the parent service worker makes.
 (function(global) {
   'use strict'
-  global.toolbox.get('/(.*)', global.toolbox.fastest);
+  global.toolbox.router.get('/(.*)', global.toolbox.fastest);
   // global.toolbox.router.get('/', global.toolbox.fastest)
   // global.toolbox.router.get('/details', global.toolbox.fastest)
   // global.toolbox.router.get('/layout', global.toolbox.fastest)
   // global.toolbox.router.get('/header', global.toolbox.fastest)
   // global.toolbox.router.get('/shots', global.toolbox.fastest)
   // global.toolbox.router.get('/index.html', global.toolbox.fastest)
-  // global.toolbox.router.get('/(.*)', global.toolbox.fastest, {
-  //   origin: /\.(?:googleapis|gstatic|bootstrapcdn|dribbble)\.com$/
-  // })
-  // global.toolbox.router.get('/(.+)', global.toolbox.fastest, {
-  // 	  origin: 'https://cloudfront.net'
-  // })
-  // global.toolbox.router.get('/(.+)', global.toolbox.fastest, {
-  // 	  origin: 'https://api.dribbble.com'
-  // })
+  global.toolbox.router.get('/(.*)', global.toolbox.fastest, {
+    origin: /\.(?:googleapis|gstatic|bootstrapcdn|dribbble)\.com$/
+  })
+  global.toolbox.router.get('/(.+)', global.toolbox.fastest, {
+  	  origin: 'https://cloudfront.net'
+  })
+  global.toolbox.router.get('/(.+)', global.toolbox.fastest, {
+  	  origin: 'https://api.dribbble.com'
+  })
 })(self)
